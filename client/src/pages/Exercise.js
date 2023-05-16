4609366b936e962308e363c6af296129feee6980
Christopher Snyder <snyder28139_04@hotmail.com>
2023-05-16 10:54:40 -0400

    added exercise queries

--- client/src/pages/Exercise.js
+++ client/src/pages/Exercise.js
@@ -4,21 +4,21 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
+// import CommentList from '../components/CommentList';
+// import CommentForm from '../components/CommentForm';
 
-import { QUERY_SINGLE_THOUGHT } from '../utils/queries';
+import { QUERY_SINGLE_EXERCISE } from '../utils/queries';
 
-const SingleThought = () => {
+const SingleExercise = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
-  const { thoughtId } = useParams();
+  const { exerciseId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, {
+  const { loading, data } = useQuery(QUERY_SINGLE_EXERCISE, {
     // pass URL parameter
-    variables: { thoughtId: thoughtId },
+    variables: { exerciseId: exerciseId },
   });
 
-  const thought = data?.thought || {};
+  const exercise = data?.exercise || {};
 
   if (loading) {
     return <div>Loading...</div>;
@@ -26,9 +26,9 @@ const SingleThought = () => {
   return (
     <div className="my-3">
       <h3 className="card-header bg-dark text-light p-2 m-0">
-        {thought.thoughtAuthor} <br />
+        {exercise.name} <br />
         <span style={{ fontSize: '1rem' }}>
-          had this thought on {thought.createdAt}
+          was completed on {exercise.date}
         </span>
       </h3>
       <div className="bg-light py-4">
@@ -41,18 +41,19 @@ const SingleThought = () => {
             lineHeight: '1.5',
           }}
         >
-          {thought.thoughtText}
+          {exercise.description}
+          {exercise.type}
         </blockquote>
       </div>
 
-      <div className="my-5">
+      {/* <div className="my-5">
         <CommentList comments={thought.comments} />
       </div>
       <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
         <CommentForm thoughtId={thought._id} />
-      </div>
+      </div> */}
     </div>
   );
 };
 
-export default SingleThought;
+export default SingleExercise;
