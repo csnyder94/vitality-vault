159bc6368c8a65494e88cc2f5598ce46ba92deb2
Christopher Snyder <snyder28139_04@hotmail.com>
2023-05-17 16:46:10 -0400

    fixed previous commit

--- client/src/components/UserDataForm/index.js
+++ client/src/components/UserDataForm/index.js
@@ -1,118 +1,145 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
+import { UPDATE_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth'
 
-import { ADD_EXERCISE } from '../../utils/mutations';
-import { QUERY_EXERCISES, QUERY_ME } from '../../utils/queries';
+const UserDataForm = (props) => {
+	const [age, setAge] = useState(props?.age || 0);
+	const [height, setHeight] = useState(props?.height || 0);
+	const [weight, setWeight] = useState(props?.weight || 0);
+	const [BMI, setBMI] = useState(props?.bmi || 0);
+	const [neck, setNeck] = useState(props?.neck || 0);
+	const [chest, setChest] = useState(props?.chest || 0);
+	const [waist, setWaist] = useState(props?.waist || 0);
 
-import Auth from '../../utils/auth';
-
-const ExerciseForm = () => {
-	const [name, setExerciseName] = useState('');
-	const [description, setExerciseDescription] = useState('');
-	const [type, setExerciseType] = useState('');
-
-	const [addExercise, { error }] = useMutation(ADD_EXERCISE, {
-		update(cache, { data: { addExercise } }) {
-			try {
-				const { exercises } = cache.readQuery({ query: QUERY_EXERCISES });
-
-				cache.writeQuery({
-					query: QUERY_EXERCISES,
-					data: { exercises: [addExercise, ...exercises] },
-				});
-			} catch (e) {
-				console.error(e);
-			}
-		},
-	});
-
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
-
+	const [updateUser, { error, data }] = useMutation(UPDATE_USER);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 		try {
-			await addExercise({
+			const userData = {
+				age: Number(age),
+				height: Number(height),
+				weight: Number(weight),
+				bmi: Number(BMI),
+				neck: Number(neck),
+				chest: Number(chest),
+				waist: Number(waist)
+			};
+
+			const info = {
+				userData: userData
+			}
+			console.log(userData);
+			const { data } = await updateUser({
 				variables: {
-					name,
-					description,
-					type,
-				},
+					...info
+				}
 			});
+			// Auth.login(data.login.token)
+			console.log(data);
+		} catch (error) {
+		}
+	}
+	const handleInputChange = (e) => {
+		const { target } = e;
+		const inputType = target.name;
+		const inputValue = target.value;
 
-			setExerciseName('');
-			setExerciseDescription('');
-			setExerciseType('');
-		} catch (err) {
-			console.error(err);
+		if (inputType === 'age') {
+			setAge(inputValue);
+		} else if (inputType === 'height') {
+			setHeight(inputValue);
+		} else if (inputType === 'weight') {
+			setWeight(inputValue);
+		} else if (inputType === 'BMI') {
+			setBMI(inputValue);
+		} else if (inputType === 'neck') {
+			setNeck(inputValue);
+		} else if (inputType === 'chest') {
+			setChest(inputValue);
+		} else if (inputType === 'waist') {
+			setWaist(inputValue);
 		}
 	};
-
-	const handleChange = (event) => {
-		const { name, value } = event.target;
-
-		if (name === 'exerciseName') {
-			setExerciseName(value);
-		}
-		if (name === 'exerciseDescription') {
-			setExerciseDescription(value);
-		}
-		if (name === 'exerciseType') {
-			setExerciseType(value);
-		}
-	};
-
 	return (
-		<div>
-			{Auth.loggedIn() ? (
-				<>
-					<h3 className='text-lg font-semibold'>What exercise will you complete today?</h3>
-					<form className='flex flex-col lg:flex-row justify-between items-center' onSubmit={handleFormSubmit}>
-						<div className='w-full lg:w-3/4'>
-							<input
-								name='exerciseName'
-								placeholder='Exercise Name'
-								value={name}
-								className='w-full px-3 py-2 mb-3 border border-gray-300 rounded-md'
-								onChange={handleChange}
-							/>
-							<textarea
-								name='exerciseDescription'
-								placeholder="Here's today's exercise:"
-								value={description}
-								className='w-full px-3 py-2 mb-3 border border-gray-300 rounded-md'
-								onChange={handleChange}
-							/>
-							<input
-								name='exerciseType'
-								placeholder='Exercise Type'
-								value={type}
-								className='w-full px-3 py-2 mb-3 border border-gray-300 rounded-md'
-								onChange={handleChange}
-							/>
-						</div>
-
-						<div className='w-full lg:w-1/4'>
-							<button className='w-full py-2 text-white bg-amber-500 hover:bg-amber-600 rounded-md ml-2' type='submit'>
-								Add Exercise
-							</button>
-						</div>
-						{error && <div className='w-full my-3 p-3 bg-red-500 text-white'>{error.message}</div>}
-					</form>
-				</>
-			) : (
-				<p>
-					You need to be logged in to track your workouts. Please{' '}
-					<Link to='/login' className='text-amber-500'>
-						login
-					</Link>{' '}
-					or{' '}
-					<Link to='/signup' className='text-amber-500'>
-						signup.
-					</Link>
-				</p>
-			)}
-		</div>
+		<form className="w-full max-w-lg" onSubmit={handleSubmit}>
+			<div className="flex flex-wrap -mx-3 mb-6">
+				<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-age">
+						Age
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={age}
+						name="age"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+				<div className="w-full md:w-1/2 px-3">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-height">
+						Height
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={height}
+						name="height"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+				<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-weight">
+						Weight
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={weight}
+						name="weight"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+				<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-bmi">
+						BMI
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={BMI}
+						name="BMI"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+				<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-neck">
+						Neck
+				</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={neck}
+						name="neck"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+				<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+				<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-chest">
+						Chest
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={chest}
+						name="chest"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+				<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-waist">
+						Waist
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						value={waist}
+						name="waist"
+						onChange={handleInputChange}>
+					</input>
+				</div>
+			</div>
+			<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+			<button type='submit' className='btn btn-lg bg-amber-400 m-2'>Submit</button>
+			</div>
+		</form>
 	);
 };
 
-export default ExerciseForm;
+export default UserDataForm;
